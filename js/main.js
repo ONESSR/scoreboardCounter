3318b7de277d9997f05be7d59131c030e7d7dd90
ONESSR <ssr677@gmail.com>
2022-09-15 18:21:25 +0100

    added functionality to increment buttons.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-alert("Hello World!");
+let homeScores = 0;
+
+function addOneHome() {
+  const homeScoreDisplay = document.getElementById("homeScore");
+  homeScoreDisplay.textContent = homeScores += 1;
+}
+
+function addThreeHome() {
+  const homeScoreDisplay = document.getElementById("homeScore");
+  homeScoreDisplay.textContent = homeScores += 3;
+}
+
+function addFiveHome() {
+  const homeScoreDisplay = document.getElementById("homeScore");
+  homeScoreDisplay.textContent = homeScores += 5;
+}
+
+let guestScores = 0;
+
+function addOneGuest() {
+  const guestScoreDisplay = document.getElementById("guestScore");
+  guestScoreDisplay.textContent = guestScores += 1;
+}
+
+function addThreeGuest() {
+  const guestScoreDisplay = document.getElementById("guestScore");
+  guestScoreDisplay.textContent = guestScores += 3;
+}
+
+function addFiveGuest() {
+  const guestScoreDisplay = document.getElementById("guestScore");
+  guestScoreDisplay.textContent = guestScores += 5;
+}
